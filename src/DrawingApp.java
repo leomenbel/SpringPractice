dafcf3484a7b5c87ac9bf238c8ddad30f55f776f
Leonardo Mendez <leonardo.mendez@parchment.com>
2019-02-21 11:04:57 -0700

    how to use lists

--- src/DrawingApp.java
+++ src/DrawingApp.java
@@ -11,7 +11,7 @@ public class DrawingApp {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Triangle triangle = (Triangle) context.getBean("triangle-alias");
+        Triangle triangle = (Triangle) context.getBean("triangle");
 
         triangle.draw();
     }
